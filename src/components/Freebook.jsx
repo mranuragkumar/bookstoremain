e0863e5dba9e98d986f9c5703cb2cc13238025de
Anurag kumar <118378324+mranuragkumar@users.noreply.github.com>
2025-07-22 11:30:50 +0530

    Add files via upload

---
+++ src/components/Freebook.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import axios from "axios";
+
+function Freebook() {
+  // Filter local data
+  // const filterData = list.filter((data) => data.category === "Free");
+
+  // State for fetched data
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4000/book");
+        console.log(res.data)
+
+        // Filter fetched data
+        const data = res.data.filter((data) => data.category === "Free");
+        setBook(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div>
+          <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Accusantium veritatis alias pariatur ad dolor repudiandae eligendi
+            corporis nulla non suscipit, iure neque earum?
+          </p>
+        </div>
+
+        <div>
+          <Slider {...settings}>
+            {/* Render filtered local data */}
+            {book.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
+            {/* Render fetched data */}
+            {book.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Freebook;
